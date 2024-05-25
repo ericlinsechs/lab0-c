c58228b637fc4d7484227e0b2566ac5a89443efe
Eric Lin <ericlinsechs@gmail.com>
2024-05-25 08:32:37 +0000

    Update q_insert_head

--- queue.c
+++ queue.c
@@ -41,6 +41,23 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *new_ele = (element_t *) malloc(sizeof(*new_ele));
+    if (!new_ele)
+        return false;
+
+    new_ele->value = malloc(sizeof(char) * (strlen(s) + 1));
+    if (!new_ele->value) {
+        free(new_ele);
+        return false;
+    }
+
+    strncpy(new_ele->value, s, strlen(s) + 1);
+
+    list_add(&new_ele->list, head);
+
     return true;
 }
 
