343bc7aaf22b172755d820809ba9cd4d44745ae0
Eric Lin <ericlinsechs@gmail.com>
2024-05-30 08:05:15 +0000

    Implement q_ascend

--- queue.c
+++ queue.c
@@ -295,7 +295,28 @@ void q_sort(struct list_head *head, bool descend)
 int q_ascend(struct list_head *head)
 {
     // https://leetcode.com/problems/remove-nodes-from-linked-list/
-    return 0;
+    if (!head || list_empty(head) || list_is_singular(head))
+        return 0;
+
+iterate:
+    element_t *first, *sec;
+    bool again = false;
+
+    list_for_each_entry_safe (first, sec, head, list) {
+        if (&sec->list == head)
+            break;
+
+        if (strcmp(first->value, sec->value) >= 0) {
+            list_del(&first->list);
+            q_release_element(first);
+            again = true;
+        }
+    }
+
+    if (again)
+        goto iterate;
+
+    return q_size(head);
 }
 
 /* Remove every node which has a node with a strictly greater value anywhere to
